41146dde5d634d69f8661ba23ed3acadaefd3edb
SRubtsov <frexrub@gmail.com>
2024-06-05 12:42:39 +0300

    add get_user_id

--- src/utils.py
+++ src/utils.py
@@ -20,22 +20,33 @@ def get_user_by_apy_key(apy_key_user: str) -> Optional[User]:
     return query.scalars().first()
 
 
-def get_users_all():
-    users_schema = UserSchema(many=True)
-    users = db.session.query(User).all()
-    print("valiate", users_schema.validate(users))
-    result = users_schema.dump(users)
-    return result
-
-
 def get_users_my(apy_key_user: str) -> Tuple[User, List[User], List[User]]:
     user: Optional[User] = get_user_by_apy_key(apy_key_user)
     if user is None:
         raise UnicornException(
-                result=False,
-                error_type="Пользователь не найден",
-                error_message=f"Пользователь с ключом {apy_key_user} не найден",
-            )
+            result=False,
+            error_type="Пользователь не найден",
+            error_message=f"Пользователь с ключом {apy_key_user} не найден",
+        )
+
+    # Выгрузка данных по подпискам и подписчикам
+    query = db.session.execute(user.following)
+    following = query.scalars().all()
+    query = db.session.execute(user.followers)
+    followers = query.scalars().all()
+
+    return user, following, followers
+
+
+def get_user_id(id_user: int) -> Tuple[User, List[User], List[User]]:
+    user: Optional[User] = db.session.get(User, id_user)
+
+    if user is None:
+        raise UnicornException(
+            result=False,
+            error_type="Пользователь не найден",
+            error_message=f"Пользователь с ключом {id_user} не найден",
+        )
 
     # Выгрузка данных по подпискам и подписчикам
     query = db.session.execute(user.following)
--- src/view_users.py
+++ src/view_users.py
@@ -1,17 +1,14 @@
-from flask import Blueprint, jsonify, make_response, request
+from flask import Blueprint, make_response, request
 from flasgger import swag_from
 from typing import List, Optional
 
-from app import db
-from exceptions import UnicornException
 from models import User
 from schemas import UserSchema
-from utils import get_users_all, get_users_my
+from utils import get_users_my, get_user_id
 
 router = Blueprint('router', __name__)
 
 
-#
 @router.route("/me", methods=["GET"])
 @swag_from('swagger/get_user_me.yml', validation=False)
 def get_user_me():
@@ -19,34 +16,57 @@ def get_user_me():
     Пользователь может получить информацию о своём профиле
     :param api_key: str
         ключ пользователя
-    :return: schemas.UserOut
+    :return: UserAllSchema
         данные пользователя и статус ответа
     """
     api_key: str = request.headers.get("api-key", "test")
     me_data, following, followers = get_users_my(api_key)
 
     schema_user_following = UserSchema(many=True)
-    user_following = schema_user_following.dump(following)
+    user_following: List[User] = schema_user_following.dump(following)
 
     schema_user_followers = UserSchema(many=True)
-    user_followers = schema_user_followers.dump(followers)
+    user_followers: List[User] = schema_user_followers.dump(followers)
 
     user_info = {
         "result": True,
         "user": {
             "id": me_data.id,
             "name": me_data.name,
+            "followers": user_followers,
+            "following": user_following,
         },
-        "followers": user_followers,
-        "following": user_following,
     }
 
     return make_response(user_info, 200)
 
 
-@router.route("/all")
-def get_user_all():
-    result = get_users_all()
-    print(result)
-    return jsonify(result)
+@router.get("/<int:id>")
+@swag_from('swagger/get_user_id.yml')
+def get_user_id_(id: int):
+    """
+    Обработка запроса на получение информацию о профиле пользователя по ID
+    :param id: int
+        ID пользователя
+    :return: UserAllSchema
+        данные пользователя и статус ответа
+    """
+    me_data, following, followers = get_user_id(id)
 
+    schema_user_following = UserSchema(many=True)
+    user_following: List[User] = schema_user_following.dump(following)
+
+    schema_user_followers = UserSchema(many=True)
+    user_followers: List[User] = schema_user_followers.dump(followers)
+
+    user_info = {
+        "result": True,
+        "user": {
+            "id": me_data.id,
+            "name": me_data.name,
+            "followers": user_followers,
+            "following": user_following,
+        },
+    }
+
+    return make_response(user_info, 200)
