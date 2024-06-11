b41976cc5ac61b06b5b4cff3c9727fa30e018227
SRubtsov <frexrub@gmail.com>
2024-06-11 11:40:40 +0300

    add test_tweet

--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 
 
@@ -15,3 +17,29 @@ def test_get_user_id(client, route):
     response = client.get(route, headers=headers)
     result = response.json
     assert response.status_code == 200
+
+
+def test_get_user_id_error(client):
+    headers = {"api-key": "test"}
+    response = client.get("/api/users/10", headers=headers)
+    result = response.json
+    print(result)
+    assert response.status_code == 418
+    assert result["result"] == False
+
+
+def test_get_tweet(client):
+    headers = {"api-key": "test"}
+    response = client.get("/api/tweets", headers=headers)
+    assert response.status_code == 200
+    # assert response.json()["tweets"][0]["id"] == 1
+
+def test_post_tweet(client):
+    headers = {"api-key": "test"}
+    tweet = {"tweet_data": "Hi", "tweet_media_ids": [0]}
+    # response = client.post("/api/tweets", headers=headers, json=tweet)
+    response = client.post("/api/tweets", headers=headers, data=json.dumps(tweet))
+    result = response.json
+    print(result)
+    assert response.status_code == 201
+    assert result["tweet_id"] == 1
