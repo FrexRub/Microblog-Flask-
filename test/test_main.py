8ee992a051c148dca6a69a1c2b14da49189e1884
SRubtsov <frexrub@gmail.com>
2024-06-11 09:48:47 +0300

    add test_get_user_id

--- test/test_main.py
+++ test/test_main.py
@@ -1,6 +1,17 @@
+import pytest
+
+
 def test_get_user_me(client):
     headers = {"api-key": "test"}
     response = client.get("/api/users/me", headers=headers)
     result = response.json
     assert response.status_code == 200
     assert result["user"]["name"] == "Ivan"
+
+
+@pytest.mark.parametrize("route", ["/api/users/1", "/api/users/2", "/api/users/3", "/api/users/4"])
+def test_get_user_id(client, route):
+    headers = {"api-key": "test"}
+    response = client.get(route, headers=headers)
+    result = response.json
+    assert response.status_code == 200
