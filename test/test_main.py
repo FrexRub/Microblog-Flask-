ca3d92ef57deeecc0fd707714d6e9144ebf2cea6
SRubtsov <frexrub@gmail.com>
2024-06-11 12:46:58 +0300

    add test_likes_twee

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,3 @@
-import json
-
 import pytest
 
 
@@ -30,16 +28,41 @@ def test_get_user_id_error(client):
 
 def test_get_tweet(client):
     headers = {"api-key": "test"}
-    response = client.get("/api/tweets", headers=headers)
+    response = client.get("/api/tweets/", headers=headers)
     assert response.status_code == 200
-    # assert response.json()["tweets"][0]["id"] == 1
+
 
 def test_post_tweet(client):
     headers = {"api-key": "test"}
     tweet = {"tweet_data": "Hi", "tweet_media_ids": [0]}
-    # response = client.post("/api/tweets", headers=headers, json=tweet)
-    response = client.post("/api/tweets", headers=headers, data=json.dumps(tweet))
+    response = client.post("/api/tweets/", headers=headers, json=tweet)
     result = response.json
-    print(result)
     assert response.status_code == 201
     assert result["tweet_id"] == 1
+
+
+def test_post_likes_tweet(client):
+    headers = {"api-key": "test1"}
+    response = client.post("/api/tweets/1/likes", headers=headers, )
+    result = response.json
+    assert response.status_code == 201
+
+
+def test_delete_likes_twee_bad(client):
+    headers = {"api-key": "test"}
+    response = client.delete("/api/tweets/1/likes", headers=headers)
+    assert response.status_code == 400
+
+
+def test_delete_likes_twee(client):
+    headers = {"api-key": "test1"}
+    response = client.delete("/api/tweets/1/likes", headers=headers)
+    result = response.json
+    assert response.status_code == 200
+
+
+def test_delete_tweet(client):
+    headers = {"api-key": "test"}
+    response = client.delete("/api/tweets/1", headers=headers)
+    result = response.json
+    assert response.status_code == 200
